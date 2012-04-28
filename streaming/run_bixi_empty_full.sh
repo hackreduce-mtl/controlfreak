66bd833011a725aaabf03897595aaf2e4addd1f5
Julia Evans <julia@jvns.ca>
2012-04-28 13:30:02 -0400

    fix hadoop version in run script

--- streaming/run_bixi_empty_full.sh
+++ streaming/run_bixi_empty_full.sh
@@ -1,4 +1,4 @@
-~/hadoop/bin/hadoop jar contrib/streaming/hadoop-0.19.0-streaming.jar \                                                                                                           -mapper bixi_empty_full_mapper.py -reducer bixi_empty_full_reducer.py -file bixi_empty_full_mapper.py -file bixi_empty_full_reducer.py -input /datasets/montreal/2012.csv -output /mnt/bixidata/blah.txt
+~/hadoop/bin/hadoop jar contrib/streaming/hadoop-0.20.2-streaming.jar \                                                                                                           -mapper bixi_empty_full_mapper.py -reducer bixi_empty_full_reducer.py -file bixi_empty_full_mapper.py -file bixi_empty_full_reducer.py -input /datasets/montreal/2012.csv -output /mnt/bixidata/blah.txt
     -mapper bixi_empty_full_mapper.py \
     -reducer bixi_empty_full_reducer.py \
     -file ~/users/controlfreak/streaming/bixi_empty_full_mapper.py \
