6577f3a1cd37fd63c09a9deefe9ddfcca580dc94
Julia Evans <julia@jvns.ca>
2012-04-28 13:34:24 -0400

    try again with the streaming script

--- streaming/run_bixi_empty_full.sh
+++ streaming/run_bixi_empty_full.sh
@@ -1,8 +1,8 @@
-~/hadoop/bin/hadoop jar contrib/streaming/hadoop-0.20.2-streaming.jar \                                                                                                           -mapper bixi_empty_full_mapper.py -reducer bixi_empty_full_reducer.py -file bixi_empty_full_mapper.py -file bixi_empty_full_reducer.py -input /datasets/montreal/2012.csv -output /mnt/bixidata/blah.txt
+java -classpath ".:lib/*" org.apache.hadoop.streaming.HadoopStreaming \
     -mapper bixi_empty_full_mapper.py \
     -reducer bixi_empty_full_reducer.py \
     -file ~/users/controlfreak/streaming/bixi_empty_full_mapper.py \
     -file ~/users/controlfreak/streaming/bixi_empty_full_reducer.py \
     -input /datasets/montreal/2012.csv \
-    -output /mnt/bixidata/blah.txt
+    -output /tmp/bixidata_result.txt
 
