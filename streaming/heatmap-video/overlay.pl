89753aa93a7882771e84a5ad17d19d8e470af8eb
Kamal Marhubi <kamal@marhubi.com>
2012-05-11 20:21:33 -0300

    Produce overlay of x-y scatter onto google maps background

--- streaming/heatmap-video/overlay.pl
+++ streaming/heatmap-video/overlay.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/env perl
-use Log::Message::Simple;
+
 use File::Basename;
 use Image::Magick;
+use Log::Message::Simple;
 use strict;
 
 # debug flag
@@ -15,14 +16,21 @@ $out_dir =~ s|/$||;
 debug("Output directory: $out_dir", $debug);
 
 my $bg_filename = 'background.png';
-my $crop_w = 386;
-my $crop_h = 340;
-my $translate_x = 62;
-my $translate_y = 62;
-my $crop_geometry = $crop_w . 'x' . $crop_h . '+' . $translate_x . '+' . $translate_y;
-my $ratio = 438/495;
+
+my ($crop_w, $crop_h) = (386, 340);
+my ($translate_x, $translate_y) = (62, 62);
+my $crop_geometry = geometry($crop_w, $crop_h, $translate_x, $translate_y);
+
+my ($output_w, $output_h) = (671, 489);
+my $output_geometry = geometry($output_w, $output_h);
+
 my $overlay_width = 512;
-my $output_geometry = $overlay_width . 'x' . $ratio * $overlay_width;
+my $ratio = 438/495; # found by stretching it over google maps in image editor
+my $overlay_geometry = geometry($overlay_width,  $ratio * $overlay_width);
+
+my $out_trans_x = 255;
+my $out_trans_y = 315;
+my $out_trans_geometry = geometry(undef, undef, $out_trans_x, $out_trans_y);
 
 my $background;
 $background = Image::Magick->new;
@@ -38,7 +46,10 @@ if (-d $in_name) {
   my $dir;
   opendir($dir, $in_name) or die "can't opendir $in_name: $!";
   while (defined(my $file = readdir($dir))) {
-    process_image("$in_name/$file");
+    my $file_name = "$in_name/$file";
+    if($file_name =~ m/.png$/) {
+      process_image($file_name);
+    }
   }
   closedir($dir);
 } 
@@ -51,24 +62,55 @@ sub process_image {
   my $filename = basename($in_path);
   my $out_path = "$out_dir/$filename";
 
+  my $err;
+
   # read in the bixi bike data
-  $bixi_data->Read($in_path);
+  $err = $bixi_data->Read($in_path);
+  die $err if $err;
 
   #crop out axes
-  $bixi_data->Crop(geometry => $crop_geometry);
+  $err = $bixi_data->Crop(geometry => $crop_geometry);
+  die $err if $err;
 
   # adjust color and opacity
   my ($w, $h) = $bixi_data->Get('columns', 'rows');
-  my $overlay_geometry =  $w . 'x' . $h . "\n";
-  my $overlay = Image::Magick->new(size => $overlay_geometry);
-  $overlay->Read('canvas:red');
+  my $overlay = Image::Magick->new(size => geometry($w, $h));
+  $err = $overlay->Read('canvas:red');
+  die $err if $err;
 
-  $bixi_data->Negate();
-  $overlay->Composite(image => $bixi_data, compose => 'CopyOpacity');
+  $err = $bixi_data->Negate();
+  die $err if $err;
+  $err = $overlay->Composite(image => $bixi_data, compose => 'CopyOpacity');
+  die $err if $err;
 
-  # resize and overlay map image
-  $overlay->Resize(geometry => $output_geometry);
-  $overlay->Composite(geometry => '-66-62', image => $background, compose => 'DstOver');
+  # create background map image with correct dimensions to overlay onto
+  my $bg = Image::Magick->new;
+  $err = $bg->Read($bg_filename);
+  die $err if $err;
 
-  $overlay->Write($out_path);
+  # resize and overlay data onto map image
+  $err = $overlay->Resize(geometry => $overlay_geometry);
+  die $err if $err;
+  $err = $bg->Composite(image => $overlay, geometry => $out_trans_geometry);
+  die $err if $err;
+
+  $err = $bg->Write($out_path);
+  die $err if $err;
+
+  # clear the image objects out; unsure if this is necessary
+  @$bg = ();
+  @$overlay = ();
+  @$bixi_data = ();
+}
+
+sub geometry {
+  my $x = shift;
+  my $y = shift;
+  my $x_off = shift;
+  my $y_off = shift;
+
+  my $dims = $x . 'x' . $y if defined $x && defined $y;
+  my $offsets = sprintf "%+d%+d", $x_off, $y_off if defined $x_off && defined $y_off;
+
+  return $dims . $offsets;
 }
